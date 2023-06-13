a0f40ed776961775c3cd34018fb2cced7edfe888
satyaprasad08 <satyaprasad08@gmail.com>
2023-06-13 13:00:27 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "3.60.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
